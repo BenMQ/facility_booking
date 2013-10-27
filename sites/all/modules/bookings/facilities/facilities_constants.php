e02fd6811e3d436fc9117aee83bba669727a0289
Minqi Chen <cnchenminqi@gmail.com>
2013-10-27 18:08:00 +0800

    Initial commit for facilities module

---
+++ sites/all/modules/bookings/facilities/facilities_constants.php
@@ -0,0 +1,10 @@
+<?php
+
+define('FACILITIES_MODULE_NAME', 'facilities');
+
+// DATABASE CONSTANTS
+define('FACILITY_TABLE', 'facility');
+define('BBQ_TABLE', 'facility_bbq');
+define('SPORTS_TABLE', 'facility_sports');
+define('ROOM_TABLE', 'facility_room');
+define('BOOKING_TABLE', 'booking');
